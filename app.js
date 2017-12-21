44da6ecd062b03584aca070f00f099ae92fc1497
jktravis <joshua.travis@gmail.com>
2017-12-20 20:54:42 -0500

    Implemented use of observer

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const observer = new Observable();
 
 console.log("Starting observer");
 
-const fn1 = num => console.log('fn1: ', num);
-const fn2 = num => console.log('fn2: ', num);
+const fn1 = num => console.log('fn1: ', num + 10);
+const fn2 = num => console.log('fn2: ', num * 2);
 
 observer.subscribe(fn1);
 observer.subscribe(fn2);
@@ -13,3 +13,20 @@ const interval = setInterval(() => {
   const rnd = Math.round(Math.random() * 100);
   observer.notify(rnd);
 }, 2500);
+
+
+setInterval(() => {
+  console.log();
+  console.log("#################");
+  console.log('Adding another subscriber');
+  console.log();
+  const id = observer.subscribe((num) => console.log('fn3: ', num - 2));
+
+  setTimeout(() => {
+    console.log();
+    console.log("#################");
+    console.log('Removing last subscription');
+    console.log();
+    observer.unsubscribe(id);
+  }, 4000);
+}, 6000);
